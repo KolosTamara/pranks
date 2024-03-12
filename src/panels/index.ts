e467a386b5581b0c0f56b23605a77067d8d1ff5e
Tamara <tamarakolos.375@gmail.com>
2024-03-12 17:21:29 +0300

    fix: folder names

--- src/panels/index.ts
+++ src/panels/index.ts
@@ -1,4 +1,4 @@
-export { Persik } from './Persik';
-export { Home } from './Home';
+export { Home } from './CatFacts';
+export { Persik } from './Agify';
 
-export type { HomeProps } from './Home';
+export type { HomeProps } from './CatFacts';
